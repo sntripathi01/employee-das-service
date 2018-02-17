d764aa65861e262deefbc9acd257f6787572f253
Nand Tripathi <SACHCHIDA.NANDTRIPAT@dish.com>
2018-02-17 13:01:37 -0700

    code added

---
+++ src/main/java/com/employeedasservice/EmployeeDasServiceApplication.java
@@ -0,0 +1,12 @@
+package com.employeedasservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDasServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDasServiceApplication.class, args);
+	}
+}
